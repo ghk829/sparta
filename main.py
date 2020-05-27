16a4efce14298096863b8cf1e0bb2818fdc9a6b0
ghk829 <ghk829@hanmail.com>
2020-05-27 11:34:17 +0000

    test

---
+++ main.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+
+url = 'mongodb://messi:neymar123@ds253388.mlab.com:53388/heroku_v6l9bbkb?retryWrites=false'
+client = MongoClient(url)
+db = client['heroku_v6l9bbkb']
+collection = db['test']
+
+# collection.insert_one({'a':'b'}) C
+
+rows = collection.find({})
+collection.update_one
+collection.delete_one
+for row in rows:
+  print(row)
